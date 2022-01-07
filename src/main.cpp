f0b3b7732b3971d717681b93d512f92a875f708f
Joshua Ng <86920804+joshuang321@users.noreply.github.com>
2022-01-07 17:06:39 +0800

    Update main.cpp
    Compiled the source code and found getStrokeText and getLevelText to have some undefined behavior. Seems to be a memory leak returning memory allocated on a stack. Please recompile the source code to check for any errors.

--- src/main.cpp
+++ src/main.cpp
@@ -195,7 +195,7 @@ void loadLevel(int level)
 	}
 }
 
-const char* getStrokeText()
+std::string getStrokeText()
 {
 	int biggestStroke = 0;
 	if (balls[1].getStrokes() > balls[0].getStrokes())
@@ -208,10 +208,10 @@ const char* getStrokeText()
 	}
 	std::string s = std::to_string(biggestStroke);
 	s = "STROKES: " + s;
-	return s.c_str();
+	return s;
 }
 
-const char* getLevelText(int side)
+std::string getLevelText(int side)
 {
 	int tempLevel = (level + 1)*2 - 1;
 	if (side == 1)
@@ -220,7 +220,7 @@ const char* getLevelText(int side)
 	}
 	std::string s = std::to_string(tempLevel);
 	s = "HOLE: " + s;
-	return s.c_str();
+	return s;
 }
 
 void update()
@@ -304,9 +304,12 @@ void graphics()
 	}
 	if (state != 2)
 	{
+		std::string StrokeText = getStrokeText(), LevelText = getLevelText(0);
 		window.render(640/4 - 132/2, 480 - 32, levelTextBgTexture);
 		window.renderCenter(-160, 240 - 16 + 3, getLevelText(0), font24, black);
 		window.renderCenter(-160, 240 - 16, getLevelText(0), font24, white);
+		
+		LevelText = getLevelText(1);
 
 		window.render(640/2 + 640/4 - 132/2, 480 - 32, levelTextBgTexture);
 		window.renderCenter(160, 240 - 16 + 3, getLevelText(1), font24, black);
